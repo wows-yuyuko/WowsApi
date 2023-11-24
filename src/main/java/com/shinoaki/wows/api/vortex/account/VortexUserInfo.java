df035579f9e3324da3b3faf3f17039c31cd83fe3
JustOneSummer <myyuyukosama@gmail.com>
2023-11-24 21:46:35 +0800

    修复少个字

--- src/main/java/com/shinoaki/wows/api/vortex/account/VortexUserInfo.java
+++ src/main/java/com/shinoaki/wows/api/vortex/account/VortexUserInfo.java
@@ -25,7 +25,7 @@ public record VortexUserInfo(
             if (data != null) {
                 var hidden = data.get("hidden_profile");
                 if (hidden != null && hidden.asBoolean()) {
-                    throw new BasicException(HttpThrowableStatus.HIDDEN, accountId + "用户隐藏了战!");
+                    throw new BasicException(HttpThrowableStatus.HIDDEN, accountId + "用户隐藏了战绩!");
                 }
                 String name = data.get("name").asText();
                 double createAt = data.get("created_at").asDouble();
